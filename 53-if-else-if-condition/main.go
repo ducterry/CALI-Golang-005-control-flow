b500f54e4417c1569e8591942d22d6a5fea7bfaa
Nguyen Dang Duc <dev.ndangduc.bn@gmail.com>
2021-07-19 15:08:54 +0700

    update code 53

---
+++ 53-if-else-if-condition/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+/*
+	- Ngày: 19.07.2021
+	- By: Nguyễn Đăng Đức
+*/
+func main() {
+	var BMI = 21.0
+	if BMI < 18.5 {
+		fmt.Println("You are underweight")
+	} else if BMI >= 18.5 && BMI < 25.0 {
+		fmt.Println("Your weight is normal")
+	} else if BMI >= 25.0 && BMI < 30.0 {
+		fmt.Println("You're overweight")
+	} else {
+		fmt.Println("You're obese")
+	}
+}
